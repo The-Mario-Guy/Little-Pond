ff821f8bf071ceea6dc0ac8992f024828a5c198e
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2023-10-10 12:53:35 -0700

    Fix NPC animations

---
+++ Little Pond 1998/Assets/Scripts/Animation.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    public Texture[] framesTextures;
+    public int frames;
+    private readonly float delayBetweenInputs = 0.1f;
+    private float t;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animationStuff();
+       
+    }
+
+
+    void animationStuff()
+    {
+
+        {
+            frames++;
+            frames %= framesTextures.Length;
+            GetComponent<Renderer>().material.mainTexture = framesTextures[frames];
+            t = delayBetweenInputs;
+        }
+        
+
+
+    }
+}
