4c06fff98e5935b2dce5be4e2feaa097aea2dec1
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2023-11-29 12:06:19 -0800

    E

--- Little Pond 1998/Assets/Scripts/MainPlayerController.cs
+++ Little Pond 1998/Assets/Scripts/MainPlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MainPlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float _moveSpeed = 20;
+    public Rigidbody _rigidBody;
     void Start()
     {
         
@@ -13,6 +15,14 @@ public class MainPlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+    void Movement()
+    {
+        float xValue = Input.GetAxis("Horizontal") * _moveSpeed * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * _moveSpeed * Time.deltaTime;
+
+        transform.Translate(xValue, 0f, zValue);
+
     }
 }
